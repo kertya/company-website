9b488de69f0eb415727f08afc3d5648c66c2f011
kertya <lizakozij@gmail.com>
2025-06-12 18:39:52 +0300

    Git.ignore and company files

--- company_site/urls.py
+++ company_site/urls.py
@@ -1,22 +1,11 @@
-"""
-URL configuration for company_site project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from services import views as service_views
+from customers import views as customer_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('services/', service_views.service_list, name='service_list'),
+    path('services/<int:pk>/', service_views.service_detail, name='service_detail'),
+    path('dashboard/', customer_views.client_dashboard, name='dashboard'),
 ]
